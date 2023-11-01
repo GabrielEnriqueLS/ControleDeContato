251b57d47cf9bcf1c66d696523bd255ed657a9ca
Gabriel Enrique <gabrilenrique1906@gmail.com>
2023-11-01 06:49:26 -0300

    Eu so quero dormi

--- src/main/java/br/com/gabriel/ControledeContatos/service/interfaces/ContatosServiceInterface.java
+++ src/main/java/br/com/gabriel/ControledeContatos/service/interfaces/ContatosServiceInterface.java
@@ -1,8 +1,13 @@
 package br.com.gabriel.ControledeContatos.service.interfaces;
 
+import java.util.List;
+import java.util.Optional;
+
 import br.com.gabriel.ControledeContatos.model.Contatos;
 
 public interface ContatosInterfaces {
+
+
 
 
 }
--- src/main/java/br/com/gabriel/ControledeContatos/service/interfaces/PessoasInterfaces.java
+++
@@ -1,6 +0,0 @@
-package br.com.gabriel.ControledeContatos.service.interfaces;
-
-import br.com.gabriel.ControledeContatos.model.Pessoa;
-public interface PessoasInterfaces {
-
-}
